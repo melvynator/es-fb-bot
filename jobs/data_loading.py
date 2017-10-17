676e09c715e3eeab51e7b2f7ab9f39e2174c473b
Melvyn Peignon <peignonmel@eisti.eu>
2017-10-18 02:36:59 +0800

    use ssl

--- jobs/data_loading.py
+++ jobs/data_loading.py
@@ -15,7 +15,7 @@ host = bonsai.replace('https://%s:%s@' % (auth[0], auth[1]), '')
 es_header = [{
   'host': host,
   'port': 443,
-  'use_ssl': False,
+  'use_ssl': True,
   'http_auth': (auth[0],auth[1])
 }]
 
