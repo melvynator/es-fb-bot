7bb5d2f07d3f6e46f415b710cad5d7057f00a246
Melvyn Peignon <peignonmel@eisti.eu>
2017-10-20 12:30:48 +0800

    Fixed bug

--- app.py
+++ app.py
@@ -138,7 +138,7 @@ def send_message(recipient_id, messages):
                     }
                 }
             }
-        r = requests.post("https://graph.facebook.com/v2.6/me/messages", params=params, headers=headers, data=data)
+        r = requests.post("https://graph.facebook.com/v2.6/me/messages", params=params, headers=headers, data=json.dump(data))
         if r.status_code != 200:
             log(r.status_code)
             log(r.text)
