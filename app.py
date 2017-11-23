cb9eb2028a85dcba43e6eeb336a0084c8bb219d6
Melvyn Peignon <peignonmel@eisti.eu>
2017-11-23 13:22:27 +0000

    Add some logs for the get requests

--- app.py
+++ app.py
@@ -36,7 +36,7 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET'])
 def verify():
-    print(request.header)
+    log(request.header)
     if request.args.get("hub.mode") == "subscribe" and request.args.get("hub.challenge"):
         if not request.args.get("hub.verify_token") == os.environ["VERIFY_TOKEN"]:
             return "Verification token mismatch", 403
