c41e6d7258fa4538b11e97792bd72446cc0338fa
Melvyn Peignon <peignonmel@eisti.eu>
2017-11-23 13:17:10 +0000

    Add logs

--- app.py
+++ app.py
@@ -36,7 +36,7 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET'])
 def verify():
-    print(request)
+    print(request.header)
     if request.args.get("hub.mode") == "subscribe" and request.args.get("hub.challenge"):
         if not request.args.get("hub.verify_token") == os.environ["VERIFY_TOKEN"]:
             return "Verification token mismatch", 403
